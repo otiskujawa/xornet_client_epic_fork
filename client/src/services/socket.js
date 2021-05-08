a2ad5c58adf46237cf3063e42457b6b83cfea546
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-08 11:46:48 +0300

    first version of reporter

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('ws://localhost:8080');
+const socket = io('ws://localhost:8080', {reconnect: true});
 
 socket.on('connection', function() {
     console.log("[MESSAGE WS] connected")
