99e91e94a3be1ea1fab2674dd25a77598eee2c01
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-08 06:14:20 +0300

    moved gaugeField to its own component

---
+++ client/src/components/gaugeField.vue
@@ -0,0 +1,71 @@
+<template>
+  <fieldset class="vm">
+    <legend>{{ vm.name }}</legend>
+    <gauge
+      :icon="require('@/assets/icons/cpu.png')"
+      suffix="%"
+      :value="parseFloat(vm.cpu)"
+    />
+    <gauge
+      :icon="require('@/assets/icons/ram.png')"
+      suffix="GB"
+      :value="parseFloat(vm.ram)"
+      :maxValue="64"
+    />
+    <gauge
+      :icon="require('@/assets/icons/upload.png')"
+      suffix="mbps"
+      :value="parseFloat(vm.upload)"
+      :maxValue="1000"
+    />
+    <gauge
+      :icon="require('@/assets/icons/download.png')"
+      suffix="mbps"
+      :value="parseFloat(vm.download)"
+      :maxValue="1000"
+    />
+  </fieldset>
+</template>
+
+<script>
+import gauge from '@/components/gauge';
+
+
+export default {
+    name: 'gaugeField',
+    components: {
+        gauge,
+    },
+    props: {
+        vm: { type: Object, required: true},
+    }
+};
+</script>
+
+<style scoped>
+
+.vm {
+  display: flex;
+  flex-direction: row;
+  padding: 0px 16px 20px 16px;
+
+  border: 1px solid var(--dark-lighter);
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: var(--dark-light);
+}
+
+.vm legend {
+  left: 24px;
+  color: white;
+  text-transform: lowercase;
+}
+
+.vm:hover {
+  border: 1px solid;
+  border-radius: 4px;
+  border-image-slice: 1;
+  border-image-source: linear-gradient(to left, #DB00FF 8.32%, #4D9FFF 89.64%);
+}
+
+</style>
--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,25 +1,19 @@
 <template>
   <div class="view home">
     <logo/>
-    <fieldset v-for="vm of vms" :key="vm" class="vm">
-      <legend>{{vm.name}}</legend>
-      <gauge :icon="require('@/assets/icons/cpu.png')" suffix="%" :value="parseFloat(vm.cpu)"/>
-      <gauge :icon="require('@/assets/icons/ram.png')" suffix="GB" :value="parseFloat(vm.ram)" :maxValue="64"/>
-      <gauge :icon="require('@/assets/icons/upload.png')" suffix="mbps" :value="parseFloat(vm.upload)" :maxValue="1000"/>
-      <gauge :icon="require('@/assets/icons/download.png')" suffix="mbps" :value="parseFloat(vm.download)" :maxValue="1000"/>
-    </fieldset> 
+    <gaugeField :vm="vm" v-for="vm of vms" :key="vm"/>
   </div>
 </template>
 
 <script>
 import socket from '@/services/socket.js';
 import logo from '@/components/logo';
-import gauge from '@/components/gauge';
+import gaugeField from '@/components/gaugeField';
 
 export default {
   name: 'home',
   components: {
-    gauge,
+    gaugeField,
     logo
   },
   data: () => {
@@ -37,29 +31,7 @@ export default {
 </script>
 
 <style scoped>
-.vm {
-  display: flex;
-  flex-direction: row;
-  padding: 0px 16px 20px 16px;
 
-  border: 1px solid var(--dark-lighter);
-  border-radius: 4px;
-  cursor: pointer;
-  background-color: var(--dark-light);
-}
-
-.vm legend {
-  left: 24px;
-  color: white;
-  text-transform: lowercase;
-}
-
-.vm:hover {
-  border: 1px solid;
-  border-radius: 4px;
-  border-image-slice: 1;
-  border-image-source: linear-gradient(to left, #DB00FF 8.32%, #4D9FFF 89.64%);
-}
 
 </style>
 
