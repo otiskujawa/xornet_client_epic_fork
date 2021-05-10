aaa58244dbf2073f6773e49ff83255d2ebfc48a5
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-10 08:58:42 +0300

    favicon

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,7 +7,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/home.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/home.vue'),
+    meta: {
+      title: 'Xornet | Home'
+    }
   } 
 ]
 
@@ -15,5 +18,11 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// this sets the tab title to the current page dynamically
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.title) document.title = to.meta.title;
+  next();
+});
 
 export default router
