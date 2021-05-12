05548ebb0ea37712fa35f15d22f7a0f186dd8efa
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-12 13:35:37 +0300

    fixed server sending all reports to the reporters for no reason
    now it only sends the reports to the web clients

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,11 @@
 import io from 'socket.io-client';
 
-const socket = io('ws://backend.xornet.cloud', { reconnect: true });
+const socket = io('ws://backend.xornet.cloud', { 
+    reconnect: true,
+    auth: {
+        type: 'client',
+    },
+});
 
 socket.on('connect', function() {
     console.log("[MESSAGE WS] CONNECTED")
