ee815432afadec7f54486784a8195ab1ce1719d4
cimok2000 <cimok2000@gmail.com>
2021-05-12 23:36:22 +0100

    added disks display to the front end

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('ws://backend.xornet.cloud', { 
+const socket = io('ws://localhost:8080', { 
     reconnect: true,
     auth: {
         type: 'client',
