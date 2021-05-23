ca871b555b66381bbdaa24bfcedf5d2dd7667520
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-23 17:54:07 +0300

    small refactorings

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import app from './app.vue'
 import router from './router'
-import socket from '@/services/socket.js';
 
 createApp(app).use(router).mount('#app')
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,20 +1,12 @@
 import io from 'socket.io-client';
 
-// TODO: turn this into a class with functions that can easily talk to the server
-
 const socket = io('ws://backend.xornet.cloud', { 
     reconnect: true,
     auth: {
         type: 'client',
     },
 });
-
-socket.on('connect', function() {
-    console.log("[MESSAGE WS] CONNECTED")
-});
-
-socket.on('disconnect', function() {
-    console.log("[MESSAGE WS] DISCONNECTED")
-});
+socket.on('connect', () =>  console.log("[Socket] Connected"));
+socket.on('disconnect', () =>  console.log("[Socket] Disconnected"));
 
 export default socket;
