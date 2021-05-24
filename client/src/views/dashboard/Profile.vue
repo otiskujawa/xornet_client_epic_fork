5fcb8fca7e0c532a262909358d6b29d0e6c01a2f
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-05-25 01:53:47 +0300

    PATCH profile route
    Co-Authored-By: N1kO23 <59277828+N1kO23@users.noreply.github.com>
    Co-Authored-By: Connor <cimok2000@gmail.com>

--- client/src/views/dashboard/Profile.vue
+++ client/src/views/dashboard/Profile.vue
@@ -3,11 +3,11 @@
     <h1>Profile</h1>
     <form v-on:submit.prevent="save">
       <img class="pfp" :src="profile?.pfp ?? 'https://wallpapercave.com/wp/wp8846945.jpg'" alt="" @click="$refs.pfp.click()">
-      <input type="file" id="myFile" ref="pfp" name="filename">
+      <input type="file" id="myFile" ref="pfp" name="filename" accept="image/*">
 
       <input v-model="profile.username" class="i" type="text" placeholder="Username">
-      <input v-model="profile.email" class="i" type="email" placeholder="Email">
-      <input v-model="profile.password" class="i" type="password" placeholder="**********">
+      <!-- <input v-model="profile.email" class="i" type="email" placeholder="Email">
+      <input v-model="profile.password" class="i" type="password" placeholder="**********"> -->
       <button type="submit">SAVE</button>
     </form>
   </div>
