ee6fa5ff9a7fb7f40d3a92a85fa3826fb1a950ef
Dominik Rutter <dominik.rutter@gmail.com>
2021-06-20 05:59:16 -0400

    Stolen code courtesy of  https://qvault.io/javascript/custom-tooltip-component-in-vue/

---
+++ client/src/components/dashboard/Tooltip.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tooltip-box">
+    <slot />
+    <div class="tooltip">
+      <span class="text">{{ text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tooltip-box {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip-box:hover .tooltip {
+  opacity: 1;
+}
+
+.tooltip {
+  color: #ffffff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 2px;
+
+  width: 120px;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -60px;
+
+  opacity: 0;
+  transition: opacity 1s;
+
+  position: absolute;
+  z-index: 1;
+
+  background: #a782e8;
+}
+
+.text::after {
+  content: " ";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #a782e8 transparent transparent transparent;
+}
+</style>
