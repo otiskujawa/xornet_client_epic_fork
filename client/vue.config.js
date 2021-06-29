50b690a5da787a8c8008fd5c17b983f26c56ea67
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-06-29 04:34:43 +0300

    keep this uncommented

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-module.exports = {
-  devServer: {
-    disableHostCheck: true,
-    port: 8082,
-    public: "https://xornet.cloud",
-    https: true,
-    cert: fs.readFileSync("./cert.pem"),
-    key: fs.readFileSync("./key.pem")
-  }
-};
+// module.exports = {
+//   devServer: {
+//     disableHostCheck: true,
+//     port: 8082,
+//     public: "https://xornet.cloud",
+//     https: true,
+//     cert: fs.readFileSync("./cert.pem"),
+//     key: fs.readFileSync("./key.pem")
+//   }
+// };
