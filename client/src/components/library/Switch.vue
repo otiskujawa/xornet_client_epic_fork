56b66522758202bb1aa0ab4de16a5160c7c9f9dc
cimok2000 <cimok2000@gmail.com>
2021-06-30 15:26:34 +0100

    updated Switch component
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

--- client/src/components/library/Switch.vue
+++ client/src/components/library/Switch.vue
@@ -22,29 +22,20 @@ const active = useVModel(props, "modelValue", emit);
 <style lang="postcss" scoped>
 .track, 
 .circle {
-    @apply transition;
-    transition-duration: 100ms;
+    @apply transition duration-100;
 }
-
 .track {
     @apply w-full h-4 rounded-full top-1/2 left-0 bg-gray-500;
-
     &.active {
         @apply bg-secondary-100;
     }
 }
-
 .circle {
     @apply bg-secondary-300 w-5 h-5 rounded-full absolute top-1/2 transform -translate-y-1/2 box-content left-0;
-
-
     &:hover {
         @apply bg-secondary-400;
-        outline-width: 4px;
-        outline-style: solid;
-        outline-color: theme('colors.alpha.20');
+        box-shadow: 0 0 0 6px theme('colors.alpha.20');
     }
-
     &.active {
         @apply left-full -translate-x-full;
     }
