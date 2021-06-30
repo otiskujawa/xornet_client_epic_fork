8efec3593e799993cb874e0ea995618150da8c9b
cimok2000 <cimok2000@gmail.com>
2021-06-30 15:26:17 +0100

    added settings components
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

---
+++ client/src/components/dashboard/settings/Appearance.vue
@@ -0,0 +1,26 @@
+<template>
+  <SettingsButton icon="palette" name="Appearance" description="Change your theme & colors">
+    <button @click="isStroked()">Filled</button>
+    <Switch />
+  </SettingsButton>
+</template>
+
+<script lang="ts" setup>
+import Switch from '@/components/library/Switch.vue';
+import SettingsButton from '@/components/dashboard/SettingsButton.vue';
+
+
+
+</script>
+
+  // methods: {
+  //   isStroked() {
+  //     const isStroked = localStorage.getItem("isStroked") === "true" ? "false" : "true";
+  //     localStorage.setItem("isStroked", isStroked);
+  //   }
+  // },
+  // data() {
+  //   return {
+  //     filled: false,
+  //   }
+  // }
--- client/src/views/dashboard/Settings.vue
+++ client/src/views/dashboard/Settings.vue
@@ -1,29 +1,19 @@
 <template>
-  <div class="settingspage">
-    <button @click="isStroked()">Filled</button>
-    <Switch :name="'Filled'" v-model="filled" class="text-primary-200"/>
+  <div class="settingspage w-full h-full py-2 sm:py-6 px-2 overflow-scroll sm:px-16 md:px-32 lg:px-48 xl:px-72 2xl:128 font-workSans ">
+    <div class="flex flex-col items-center gap-2 w-full ">
+      <Account />
+      <Interface />
+      <Appearance />
+      <Language />
+      <Security />
+    </div>
   </div>
 </template>
 
-<script>
-import Switch from '@/components/library/Switch.vue';
-
-export default {
-  components: {
-    Switch,
-  },
-  methods: {
-    isStroked() {
-      const isStroked = localStorage.getItem("isStroked") === "true" ? "false" : "true";
-      localStorage.setItem("isStroked", isStroked);
-    }
-  },
-  data() {
-    return {
-      filled: false,
-    }
-  }
-};
+<script lang="ts" setup>
+import Account from '@/components/dashboard/settings/Account.vue';
+import Interface from '@/components/dashboard/settings/Interface.vue';
+import Appearance from '@/components/dashboard/settings/Appearance.vue';
+import Language from '@/components/dashboard/settings/Language.vue';
+import Security from '@/components/dashboard/settings/Security.vue';
 </script>
-
-<style scoped></style>
