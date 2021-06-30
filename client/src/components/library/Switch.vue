c7f5be52418858494b1c8c9cd67652f3d7f689e0
cimok2000 <cimok2000@gmail.com>
2021-06-30 11:07:24 +0100

    made Switch component
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

---
+++ client/src/components/library/Switch.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="relative w-12 h-6 cursor-pointer flex items-center" @click="active = !active">
+        <div class="track" :class="{active}"></div>
+        <div class="circle" :class="{active}"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmit } from "vue";
+import { useVModel } from "@vueuse/core";
+
+const props = defineProps<{
+    name: string, 
+    modelValue: boolean;
+}>();
+
+const emit = defineEmit(["update:modelValue"]);
+const active = useVModel(props, "modelValue", emit);
+
+</script>
+
+<style lang="postcss" scoped>
+.track, 
+.circle {
+    @apply transition;
+    transition-duration: 100ms;
+}
+
+.track {
+    @apply w-full h-4 rounded-full top-1/2 left-0 bg-gray-500;
+
+    &.active {
+        @apply bg-secondary-100;
+    }
+}
+
+.circle {
+    @apply bg-secondary-300 w-5 h-5 rounded-full absolute top-1/2 transform -translate-y-1/2 box-content left-0;
+
+
+    &:hover {
+        @apply bg-secondary-400;
+        outline-width: 4px;
+        outline-style: solid;
+        outline-color: theme('colors.alpha.20');
+    }
+
+    &.active {
+        @apply left-full -translate-x-full;
+    }
+}
+</style>
--- client/src/views/dashboard/Settings.vue
+++ client/src/views/dashboard/Settings.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="settingspage">
-    <div @click="isStroked()">Filled</div>
+    <button @click="isStroked()">Filled</button>
+    <Switch :name="'Filled'" v-model="filled" class="text-primary-200"/>
   </div>
 </template>
 
 <script>
+import Switch from '@/components/library/Switch.vue';
+
 export default {
+  components: {
+    Switch,
+  },
   methods: {
     isStroked() {
       const isStroked = localStorage.getItem("isStroked") === "true" ? "false" : "true";
       localStorage.setItem("isStroked", isStroked);
+    }
+  },
+  data() {
+    return {
+      filled: false,
     }
   }
 };
