921af76758884b65a11c547f413c741f1cb6cf19
cimok2000 <cimok2000@gmail.com>
2021-06-30 16:34:33 +0100

    created prefs.ts for local storage
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

--- client/src/components/dashboard/settings/Appearance.vue
+++ client/src/components/dashboard/settings/Appearance.vue
@@ -1,26 +1,11 @@
 <template>
   <SettingsButton icon="palette" name="Appearance" description="Change your theme & colors">
-    <button @click="isStroked()">Filled</button>
-    <Switch />
+    <Switch name="Stroked Icons" v-model="iconStroke" />
   </SettingsButton>
 </template>
 
 <script lang="ts" setup>
 import Switch from '@/components/library/Switch.vue';
 import SettingsButton from '@/components/dashboard/SettingsButton.vue';
-
-
-
+import { iconStroke } from '@/states/prefs';
 </script>
-
-  // methods: {
-  //   isStroked() {
-  //     const isStroked = localStorage.getItem("isStroked") === "true" ? "false" : "true";
-  //     localStorage.setItem("isStroked", isStroked);
-  //   }
-  // },
-  // data() {
-  //   return {
-  //     filled: false,
-  //   }
-  // }
