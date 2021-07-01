a3fad4966b254edc731c3a18ca2471ed2be8ace8
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-07-02 00:17:26 +0300

    cool colors depending on usage
    Co-Authored-By: Bluskript <52386117+Bluskript@users.noreply.github.com>

--- client/src/components/dashboard/CoreBars.vue
+++ client/src/components/dashboard/CoreBars.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="coreBars relative flex gap-2">
     <div class="bar flex justify-center" v-for="(value, i) in props.cores" :key="`core-${i}`">
-      <div class="track"></div>
-      <div class="progress" :style="`height: ${value}%;`"></div>
+      <div class="track h-full rounded-full bg-gray-300 w-2"></div>
+      <div class="progress rounded-full bg-gradient-to-tr from-primary-200 to-tertiary-200 transform transition-all duration-500 w-2.5 absolute bottom-0 min-h-2.5" :class="{watchOut: value >= 50, death: value >= 95}" :style="`height: ${value}%;`"></div>
     </div>
   </div>
 </template>
@@ -12,17 +12,14 @@ import { defineProps, computed } from "vue";
 const props = defineProps<{
   cores: number[]
 }>();
+
 </script>
 
 <style lang="postcss" scoped>
-.bar {
-  .progress {
-    @apply rounded-full bg-gradient-to-tr from-primary-200 to-tertiary-300 transition-all duration-500 w-2.5 absolute bottom-0 min-h-2.5;
-  }
-  .track {
-    @apply h-full rounded-full bg-gray-300 w-2;
-  }
-
+.watchOut {
+  @apply from-yellow-300 to-orange-500;
 }
-
+.death {
+  @apply from-red-600 to-purple-500;
+}
 </style>
