187b395e97219243d11d35f88f1f1b6d4f6c217e
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-07-04 16:05:42 -0700

    whatever the fuck

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,14 +1,11 @@
 const fs = require("fs");
-
-let dev = process.env.NODE_ENV === "development" ? true : false;
-
 module.exports = {
   devServer: {
     disableHostCheck: true,
     port: 8082,
-    public: !dev ? "https://xornet.cloud" : undefined,
-    https: !dev ? true : undefined,
-    cert: !dev ? fs.readFileSync("./cert.pem") : undefined,
-    key: !dev ? fs.readFileSync("./key.pem") : undefined
+    public: "https://dev.xornet.cloud",
+    https: true,
+    cert: fs.readFileSync("./cert.pem"),
+    key: fs.readFileSync("./key.pem"),
   }
 };
