b5b1b158fde2147bda1faf1b15eda89d49270515
cimok2000 <cimok2000@gmail.com>
2021-07-13 12:29:32 +0100

    added staged script
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const dev = process.env.NODE_ENV.trim() === "development" ? true : false;
+const dev = process.env.NODE_ENV.trim() === "development" || process.env.NODE_ENV.trim() === "staged" ? true : false;
 module.exports = {
   devServer: {
     disableHostCheck: true,
