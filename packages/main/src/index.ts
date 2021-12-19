74d053aa012bfb0e5af16dc4087259f473dfee9b
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-19 22:52:04 +0200

    Update index.ts

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -1,10 +1,10 @@
-import {app, BrowserWindow} from 'electron';
-import {join} from 'path';
-import {URL} from 'url';
-import './security-restrictions';
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+import { URL } from "url";
+import "./security-restrictions";
 
 const isSingleInstance = app.requestSingleInstanceLock();
-const isDevelopment = import.meta.env.MODE === 'development';
+const isDevelopment = import.meta.env.MODE === "development";
 
 if (!isSingleInstance) {
   app.quit();
@@ -15,14 +15,17 @@ app.disableHardwareAcceleration();
 
 // Install "Vue.js devtools"
 if (isDevelopment) {
-  app.whenReady()
-    .then(() => import('electron-devtools-installer'))
-    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
-      loadExtensionOptions: {
-        allowFileAccess: true,
-      },
-    }))
-    .catch(e => console.error('Failed install extension:', e));
+  app
+    .whenReady()
+    .then(() => import("electron-devtools-installer"))
+    .then(({ default: installExtension, VUEJS3_DEVTOOLS }) =>
+      installExtension(VUEJS3_DEVTOOLS, {
+        loadExtensionOptions: {
+          allowFileAccess: true,
+        },
+      })
+    )
+    .catch((e) => console.error("Failed install extension:", e));
 }
 
 let mainWindow: BrowserWindow | null = null;
@@ -30,10 +33,12 @@ let mainWindow: BrowserWindow | null = null;
 const createWindow = async () => {
   mainWindow = new BrowserWindow({
     show: false, // Use 'ready-to-show' event to show window
+    transparent: true,
+    frame: false,
     webPreferences: {
       nativeWindowOpen: true,
       webviewTag: false, // The webview tag is not recommended. Consider alternatives like iframe or Electron's BrowserView. https://www.electronjs.org/docs/latest/api/webview-tag#warning
-      preload: join(__dirname, '../../preload/dist/index.cjs'),
+      preload: join(__dirname, "../../preload/dist/index.cjs"),
     },
   });
 
@@ -43,7 +48,7 @@ const createWindow = async () => {
    *
    * @see https://github.com/electron/electron/issues/25012
    */
-  mainWindow.on('ready-to-show', () => {
+  mainWindow.on("ready-to-show", () => {
     mainWindow?.show();
 
     if (isDevelopment) {
@@ -56,17 +61,18 @@ const createWindow = async () => {
    * Vite dev server for development.
    * `file://../renderer/index.html` for production and test
    */
-  const pageUrl = isDevelopment && import.meta.env.VITE_DEV_SERVER_URL !== undefined
-    ? import.meta.env.VITE_DEV_SERVER_URL
-    : new URL('../renderer/dist/index.html', 'file://' + __dirname).toString();
-
+  const pageUrl =
+    isDevelopment && import.meta.env.VITE_DEV_SERVER_URL !== undefined
+      ? import.meta.env.VITE_DEV_SERVER_URL
+      : new URL(
+          "../renderer/dist/index.html",
+          "file://" + __dirname
+        ).toString();
 
   await mainWindow.loadURL(pageUrl);
 };
 
-
-
-app.on('second-instance', () => {
+app.on("second-instance", () => {
   // Someone tried to run a second instance, we should focus our window.
   if (mainWindow) {
     if (mainWindow.isMinimized()) mainWindow.restore();
@@ -74,24 +80,22 @@ app.on('second-instance', () => {
   }
 });
 
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-
-app.whenReady()
+app
+  .whenReady()
   .then(createWindow)
-  .catch((e) => console.error('Failed create window:', e));
-
+  .catch((e) => console.error("Failed create window:", e));
 
 // Auto-updates
 if (import.meta.env.PROD) {
-  app.whenReady()
-    .then(() => import('electron-updater'))
-    .then(({autoUpdater}) => autoUpdater.checkForUpdatesAndNotify())
-    .catch((e) => console.error('Failed check updates:', e));
+  app
+    .whenReady()
+    .then(() => import("electron-updater"))
+    .then(({ autoUpdater }) => autoUpdater.checkForUpdatesAndNotify())
+    .catch((e) => console.error("Failed check updates:", e));
 }
-
