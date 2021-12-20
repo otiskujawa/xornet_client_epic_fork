3076e849ec65413ea3f22916a82cfb3512f29397
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-20 12:36:41 +0200

    fixed a few typos

--- packages/vue/src/router.ts
+++ packages/vue/src/router.ts
@@ -2,8 +2,8 @@ import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
-  { path: "/", name: "home", component: () => import("./views/homepage.vue") },
-  { path: "/machines", name: "machines", component: () => import("./views/machines.vue") },
+  { path: "/", name: "home", component: () => import("./views/HomeView.vue") },
+  { path: "/machines", name: "machines", component: () => import("./views/MachinesView.vue") },
 ];
 
 export default createRouter({
