89ec98ab971e3860253f1b1185c8863d5aed0c5c
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-26 23:52:13 +0200

    made base tooltips :yay:
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- packages/vue/src/components/NavigationBar.vue
+++ packages/vue/src/components/NavigationBar.vue
@@ -1,17 +1,19 @@
 <template>
   <nav class="h-full w-min flex flex-col">
-    <ul>
-      <li v-for="route of routes.filter(route => !!route.icon)" :key="route.path" class="flex">
-        <router-link
-          class="p-4 flex items-center justify-center text-69px text-text opacity-25"
-          :to="route.name"
-        >
-          <icon
-            class="w-24px h-24px"
-            :name="route.icon!"
-          />
-        </router-link>
-      </li>
+    <ul> 
+      <base-tooltip :text="route.name" v-for="route of routes.filter(route => !!route.icon)" :key="route.path">
+        <li class="flex">
+          <router-link
+            class="p-4 flex items-center justify-center text-69px text-text opacity-25"
+            :to="route.name"
+          >
+            <icon
+              class="w-24px h-24px"
+              :name="route.icon!"
+            />
+          </router-link>
+        </li>
+      </base-tooltip>
     </ul>
   </nav>
 </template>
@@ -19,6 +21,7 @@
 <script setup lang="ts">
 import icon from "/@/components/shared/Icon.vue";
 import type { FancyRouteRecord } from "../router";
+import BaseTooltip from "./base/BaseTooltip.vue";
 
 defineProps<{
 	routes: FancyRouteRecord[]
