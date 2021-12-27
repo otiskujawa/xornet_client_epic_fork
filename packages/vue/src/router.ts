be89750b76fb831a608d8096a31d1d5e6f02ffe7
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-12-27 05:09:41 +0200

    update ci scripts
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- packages/vue/src/router.ts
+++ packages/vue/src/router.ts
@@ -1,5 +1,5 @@
 import type { RouteRecordRaw } from "vue-router";
-import { createMemoryHistory, createRouter, createWebHashHistory, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 export type FancyRouteRecord = RouteRecordRaw & { icon?: string };
 
