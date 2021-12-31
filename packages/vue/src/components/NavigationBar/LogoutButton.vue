c2b5c0f2cc31d35e13c7456001d32936ada70e3a
N1kO23 <niko.huuskonen.00@gmail.com>
2021-12-31 18:40:17 +0200

    Skulls for the confirm dialog
    Solves issue #9
    
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>

--- packages/vue/src/components/NavigationBar/LogoutButton.vue
+++ packages/vue/src/components/NavigationBar/LogoutButton.vue
@@ -2,18 +2,20 @@
   <base-dialog v-model="showConfirmDialog" unsized>
     <div class="bg-black flex flex-col gap-4 p-4 w-full text-center">
       <div class="flex items-center justify-center">
-        <i-fluency-skull class="w-24 h-24" />
+        <i-fluency-skull v-if="isSkullHappy==0" class="w-24 h-24" />
+        <i-fluency-happy-skull v-else-if="isSkullHappy==1" class="w-24 h-24" />
+        <i-fluency-pirate-bay v-else class="w-24 h-24" />
       </div>
       <div class="text-white">
         Are you sure you want to logout?
       </div>
       <div class="flex gap-4">
-        <base-button class="w-full" @click="showConfirmDialog = false">
+        <base-button class="w-full" @click="showConfirmDialog = false" @mouseover="isSkullHappy = 1" @mouseleave="isSkullHappy = 0">
           <i-fluency-happy-skull class="w-24px h-24px" />
           No
         </base-button>
-        <base-button transparent class="w-full bg-caution" @click="logout">
-          <i-fluency-skull class="w-24px h-24px" />
+        <base-button transparent class="w-full bg-caution" @click="logout" @mouseover="isSkullHappy = 2" @mouseleave="isSkullHappy = 0">
+          <i-fluency-pirate-bay class="w-24px h-24px" />
           Yes
         </base-button>
       </div>
@@ -37,6 +39,7 @@ import { useState } from "/@/services/state";
 const router = useRouter();
 const state = useState();
 const showConfirmDialog = ref(false);
+const isSkullHappy = ref(0);
 
 const logout = () => {
 	router.push({ name: "login" });
