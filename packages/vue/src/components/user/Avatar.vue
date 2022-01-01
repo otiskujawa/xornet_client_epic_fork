de97f5a5f0769e6029c348a86db505b018fb1dde
N1kO23 <niko.huuskonen.00@gmail.com>
2022-01-01 18:35:05 +0200

    Added avatar component

---
+++ packages/vue/src/components/user/Avatar.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="rounded-full" :src="img" alt="avatar" :style="{width: (width || 'auto'), height: (height || 'auto')}">
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+	img?: string
+	width?: string
+	height?: string
+}>();
+</script>
