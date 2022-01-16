5f3ced36ed808bdffe90547c09676e3bb823cf6b
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-01-16 18:12:38 +0200

    added raspbian icon (fix #29)

--- packages/vue/src/components/shared/DistroIcon.vue
+++ packages/vue/src/components/shared/DistroIcon.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="distro-icon flex items-center">
     <i-distros-alpine v-if="name?.toLowerCase().includes('alpine')" />
-    <i-distros-arch v-if="name?.toLowerCase().includes('arch')" />
-    <i-distros-debian v-if="name?.toLowerCase().includes('debian')" />
-    <i-distros-redhat v-if="name?.toLowerCase().includes('red hat')" />
-    <i-distros-ubuntu v-if="name?.toLowerCase().includes('ubuntu')" />
-    <i-distros-openwrt v-if="name?.toLowerCase().includes('openwrt')" />
-    <i-distros-suse v-if="name?.toLowerCase().includes('suse')" />
-    <i-distros-endeavour v-if="name?.toLowerCase().includes('endeavour')" />
-    <i-distros-garuda v-if="name?.toLowerCase().includes('garuda')" />
-    <i-distros-macos v-if="name?.toLowerCase().includes('macos')" />
-    <i-distros-windows10 v-if="name?.toLowerCase().includes('windows')" />
+    <i-distros-arch v-else-if="name?.toLowerCase().includes('arch')" />
+    <i-distros-debian v-else-if="name?.toLowerCase().includes('debian')" />
+    <i-distros-redhat v-else-if="name?.toLowerCase().includes('red hat')" />
+    <i-distros-ubuntu v-else-if="name?.toLowerCase().includes('ubuntu')" />
+    <i-distros-openwrt v-else-if="name?.toLowerCase().includes('openwrt')" />
+    <i-distros-suse v-else-if="name?.toLowerCase().includes('suse')" />
+    <i-distros-endeavour v-else-if="name?.toLowerCase().includes('endeavour')" />
+    <i-distros-garuda v-else-if="name?.toLowerCase().includes('garuda')" />
+    <i-distros-raspbian v-else-if="name?.toLowerCase().includes('raspbian')" />
+    <i-distros-macos v-else-if="name?.toLowerCase().includes('macos')" />
+    <i-distros-windows10 v-else-if="name?.toLowerCase().includes('windows')" />
+    <h1 v-else>
+      {{ name?.toLowerCase() }}
+    </h1>
   </div>
 </template>
 
