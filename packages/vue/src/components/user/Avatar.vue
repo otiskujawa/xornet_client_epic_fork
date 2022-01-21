8348f28d95068b318470a1053c8f3034a2cc75c4
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-01-21 03:27:30 +0200

    set a default avatar

--- packages/vue/src/components/user/Avatar.vue
+++ packages/vue/src/components/user/Avatar.vue
@@ -1,13 +1,14 @@
 <template>
   <div v-if="user" class="rounded-full">
     <router-link v-if="user.uuid" draggable="false" :to="{ name: 'profile', params: { uuid: user.uuid }}">
-      <img class="rounded-full object-cover" :src="user.avatar" :alt="user.username" style="aspect-ratio: 1/1">
+      <img class="rounded-full object-cover" :src="user.avatar || defaultAvatar" :alt="user.username" style="aspect-ratio: 1/1">
     </router-link>
-    <img v-else class="object-cover" :src="user.avatar" :alt="user.username" style="aspect-ratio: 1/1">
+    <img v-else class="object-cover" :src="user.avatar || defaultAvatar" :alt="user.username" style="aspect-ratio: 1/1">
   </div>
 </template>
 <script setup lang="ts">
 import type { IUser } from "types/api/user";
+const defaultAvatar = "https://cdn.discordapp.com/attachments/755597803102928966/933895255508525177/unknown.png";
 defineProps<{
 	user?: IUser
 }>();
