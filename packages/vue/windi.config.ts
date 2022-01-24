c03d81301bd509303c0b94b802cbc2a65814577c
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-01-24 16:06:36 +0200

    added 100gbit color & refactored windi config

--- packages/vue/windi.config.ts
+++ packages/vue/windi.config.ts
@@ -7,10 +7,10 @@ import typography from "windicss/plugin/typography";
  */
 const withOpacityValue = (cssVariable: string) => ({ opacityVariable, opacityValue }) => {
 	if (opacityValue) {
-		if (opacityVariable) return `rgba(var(--${cssVariable}), var(${opacityVariable}, 1))`;
-		return `rgba(var(--${cssVariable}), ${opacityValue})`;
+		if (opacityVariable) return `rgba(var(--color-${cssVariable}), var(${opacityVariable}, 1))`;
+		return `rgba(var(--color-${cssVariable}), ${opacityValue})`;
 	}
-	return `rgb(var(--${cssVariable}))`;
+	return `rgb(var(--color-${cssVariable}))`;
 };
 
 export default defineConfig({
@@ -19,27 +19,28 @@ export default defineConfig({
 	theme: {
 		extend: {
 			colors: {
-				"text": withOpacityValue("color-text"),
-				"caution": withOpacityValue("color-caution"),
-				"active": withOpacityValue("color-active"),
-				"100mbps": withOpacityValue("color-100mbps"),
-				"1000mbps": withOpacityValue("color-1000mbps"),
-				"10000mbps": withOpacityValue("color-10000mbps"),
+				"text": withOpacityValue("text"),
+				"caution": withOpacityValue("caution"),
+				"active": withOpacityValue("active"),
+				"100mbps": withOpacityValue("100mbps"),
+				"1000mbps": withOpacityValue("1000mbps"),
+				"10000mbps": withOpacityValue("10000mbps"),
+				"100000mbps": withOpacityValue("100000mbps"),
 				"background": {
-					200: withOpacityValue("color-background-200"),
-					300: withOpacityValue("color-background-300"),
-					400: withOpacityValue("color-background-400"),
-					500: withOpacityValue("color-background-500"),
-					600: withOpacityValue("color-background-600"),
+					200: withOpacityValue("background-200"),
+					300: withOpacityValue("background-300"),
+					400: withOpacityValue("background-400"),
+					500: withOpacityValue("background-500"),
+					600: withOpacityValue("background-600"),
 				},
 				"tooltip": {
-					background: withOpacityValue("color-tooltip-background"),
-					text: withOpacityValue("color-tooltip-text"),
+					background: withOpacityValue("tooltip-background"),
+					text: withOpacityValue("tooltip-text"),
 				},
 				"primary": {
-					300: withOpacityValue("color-primary-300"),
-					400: withOpacityValue("color-primary-400"),
-					500: withOpacityValue("color-primary-500"),
+					300: withOpacityValue("primary-300"),
+					400: withOpacityValue("primary-400"),
+					500: withOpacityValue("primary-500"),
 				},
 			},
 		},
