84fed7021e7864a14f2c7f702eaf6a98c7b3e00a
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-02 21:26:53 -0500

    improved performance of the sound manager a bit
    Co-Authored-By: Blusk <52386117+Bluskript@users.noreply.github.com>

--- packages/vue/src/services/SoundManager.ts
+++ packages/vue/src/services/SoundManager.ts
@@ -1,33 +1,36 @@
 import { state } from "./state";
 
 export class SoundManager {
-	public static playHover() {
-		if (!state.settings.enableSoundEffects.value) return;
-
-		const sounds = [
+	public static sounds = {
+		hoverSounds: [
 			new Audio("sounds/hover1.wav"),
 			new Audio("sounds/hover2.wav"),
 			new Audio("sounds/hover3.wav"),
 			new Audio("sounds/hover4.wav"),
-		];
+		],
+		click1: new Audio("sounds/click1.wav"),
+		escape: new Audio("sounds/escape.wav"),
+	};
 
-		const sound = sounds[~~(Math.random() * sounds.length - 1)];
+	public static playHover() {
+		if (!state.settings.enableSoundEffects.value) return;
+		const sounds = this.sounds.hoverSounds;
+		let sound = sounds[~~(Math.random() * sounds.length - 1)];
+		sound = <HTMLAudioElement>sound.cloneNode(false);
 		sound.volume = 0.15;
 		sound.play();
 	}
 
 	public static playClick() {
 		if (!state.settings.enableSoundEffects.value) return;
-
-		const sound = new Audio("sounds/click1.wav");
+		const sound = <HTMLAudioElement> this.sounds.click1.cloneNode(false);
 		sound.volume = 0.25;
 		sound.play();
 	}
 
 	public static playEscape() {
 		if (!state.settings.enableSoundEffects.value) return;
-
-		const sound = new Audio("sounds/escape.wav");
+		const sound = <HTMLAudioElement> this.sounds.escape.cloneNode(false);
 		sound.volume = 0.25;
 		sound.play();
 	}
