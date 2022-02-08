4cd24d4a94b0a2f80e017a8ebae19cc8d55c227d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-08 07:45:49 -0500

    implemented base command palette code

--- packages/vue/src/components/CommandPallete.vue
+++ packages/vue/src/components/CommandPallete.vue
@@ -1,28 +1,75 @@
 <template>
   <base-dialog v-model="state.window.isShowingCommandPallete">
-    <div class="flexcol gap-2 fixed bg-background-300 z-50 text-text w-128 p-4 rounded-8px center">
-      <input ref="commandPalleteInput" v-model="search" class="bg-transparent text-lg" placeholder="Quick search...">
-      <div class="h-1px bg-background-600 w-full" />
-      <p class="opacity-50">
-        Routes
-      </p>
+    <div class="border-2 border-background-200 flexcol gap-2 fixed bg-background-300 z-50 text-text w-128 p-2 rounded-8px center">
+      <div class="p-2 flexcol gap-4">
+        <input ref="commandPalleteInput" v-model="search" class="bg-transparent placeholder-text placeholder-opacity-50 text-lg" placeholder="Quick search...">
+        <div class="h-1px bg-background-200 w-full" />
+      </div>
+
+      <div v-for="category of new Set(filteredCommandPaletteOptions.map(option => option.category))" :key="category" class="flexcol">
+        <p class="opacity-50 ml-4 mb-2 capitalize">
+          {{ category }}s
+        </p>
+        <button
+          v-for="option of filteredCommandPaletteOptions.filter(option => option.category === category)"
+          :key="option.title"
+          class="capitalize items-center flex gap-4 p-4 py-3 rounded-6px hover:bg-background-200 text-left"
+          @click="option.onClickHandler && option.onClickHandler(); state.window.isShowingCommandPallete = false;"
+        >
+          <fluency-icon :icon="option.icon" />
+          {{ option.title }}
+        </button>
+      </div>
     </div>
   </base-dialog>
 </template>
 
 <script setup lang="ts">
 import type { Ref } from "vue";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
+
+import { useRouter } from "vue-router";
 import { useState } from "../services/state";
 import BaseDialog from "./base/BaseDialog.vue";
+export type CommandPalleteOptionCategory = "route" | "machine";
+const router = useRouter();
 const search = ref("");
 const state = useState();
 const commandPalleteInput = ref() as Ref<HTMLInputElement>;
 onMounted(() => {
 	commandPalleteInput.value.focus();
 });
+
+interface CommandPalleteOption {
+	title: string
+	icon?: string
+	category: CommandPalleteOptionCategory
+	onClickHandler?: Function
+}
+
+const commandPaletteOptions: CommandPalleteOption[] = [
+	{
+		onClickHandler: () => router.push({ name: "machines" }),
+		category: "route",
+		title: "machines",
+		icon: "nas",
+	},
+	{
+		onClickHandler: () => router.push({ name: "settings" }),
+		category: "route",
+		title: "settings",
+		icon: "settings",
+	},
+	{
+		onClickHandler: () => router.push({ name: "profile" }),
+		category: "route",
+		title: "profile",
+		icon: "user",
+	},
+];
+
+const filteredCommandPaletteOptions = computed(() => {
+	return commandPaletteOptions.filter(option => option.title.includes(search.value.toLowerCase()));
+});
+
 </script>
-
-<style scoped lang="postcss">
-
-</style>
---
+++ packages/vue/src/components/shared/FluencyIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="icon flex items-center">
+    <i-fluency-active-state v-if="icon === 'active-state'" />
+    <i-fluency-at-sign v-if="icon === 'at-sign'" />
+    <i-fluency-clock v-if="icon === 'clock'" />
+    <i-fluency-close v-if="icon === 'close'" />
+    <i-fluency-computer v-if="icon === 'computer'" />
+    <i-fluency-country v-if="icon === 'country'" />
+    <i-fluency-done v-if="icon === 'done'" />
+    <i-fluency-down v-if="icon === 'down'" />
+    <i-fluency-github v-if="icon === 'github'" />
+    <i-fluency-happy-skull v-if="icon === 'happy-skull'" />
+    <i-fluency-ipv6 v-if="icon === 'ipv6'" />
+    <i-fluency-lightning-bolt v-if="icon === 'lightning-bolt'" />
+    <i-fluency-logs v-if="icon === 'logs'" />
+    <i-fluency-maximize v-if="icon === 'maximize'" />
+    <i-fluency-memory v-if="icon === 'memory'" />
+    <i-fluency-minimize v-if="icon === 'minimize'" />
+    <i-fluency-nas v-if="icon === 'nas'" />
+    <i-fluency-notes v-if="icon === 'notes'" />
+    <i-fluency-ping v-if="icon === 'ping'" />
+    <i-fluency-pirate-bay v-if="icon === 'pirate-bay'" />
+    <i-fluency-plus v-if="icon === 'plus'" />
+    <i-fluency-processes v-if="icon === 'processes'" />
+    <i-fluency-processor v-if="icon === 'processor'" />
+    <i-fluency-restart v-if="icon === 'restart'" />
+    <i-fluency-select-column v-if="icon === 'select-column'" />
+    <i-fluency-services v-if="icon === 'services'" />
+    <i-fluency-settings v-if="icon === 'settings'" />
+    <i-fluency-skull v-if="icon === 'skull'" />
+    <i-fluency-speedometer v-if="icon === 'speedometer'" />
+    <i-fluency-status v-if="icon === 'status'" />
+    <i-fluency-switch v-if="icon === 'switch'" />
+    <i-fluency-tag v-if="icon === 'tag'" />
+    <i-fluency-tasks v-if="icon === 'tasks'" />
+    <i-fluency-temperature v-if="icon === 'temperature'" />
+    <i-fluency-terminal v-if="icon === 'terminal'" />
+    <i-fluency-thor-hammer v-if="icon === 'thor-hammer'" />
+    <i-fluency-trash v-if="icon === 'trash'" />
+    <i-fluency-unmaximize v-if="icon === 'unmaximize'" />
+    <i-fluency-up v-if="icon === 'up'" />
+    <i-fluency-upgrade v-if="icon === 'upgrade'" />
+    <i-fluency-user v-if="icon === 'user'" />
+    <i-fluency-video-card v-if="icon === 'video-card'" />
+    <i-fluency-zap v-if="icon === 'zap'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ icon: string }>();
+</script>
