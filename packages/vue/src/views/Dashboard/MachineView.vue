6f77351f33d86f5ae4efd64c0181bae71d626329
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-02-11 16:39:36 -0500

    added drives

--- packages/vue/src/views/Dashboard/MachineView.vue
+++ packages/vue/src/views/Dashboard/MachineView.vue
@@ -1,8 +1,30 @@
 <template>
-  <div v-if="machine" class="flexcol  gap-4 bg-background-200 w-full">
+  <div v-if="machine" class="flexcol bg-background-200 w-full">
     <machine-header :machine="machine" />
-    <div class="p-4">
-      <network-switch v-if="machine.network" :interfaces="machine.network" />
+    <div class="flexcol gap-2 px-4">
+      <div v-for="disk of machine.disks" :key="disk.mount" class="flex items-center gap-2">
+        <i-fluency-hdd v-if="disk.type === 'HDD'" class="text-2xl" />
+        <i-fluency-ssd v-else class="text-2xl" />
+        <div class="flexcol justify-between min-w-48">
+          <div class="flex justify-between items-center">
+            <p>
+              {{ disk.mount }}
+            </p>
+            <p>
+              {{ ( disk.used / disk.total * 100).toFixed(2) }}% used
+            </p>
+          </div>
+          <div class="opacity-20 flex justify-between items-center">
+            <p>
+              {{ `${(disk.used / 1024 / 1024 / 1024).toFixed(2)} GB` }}
+            </p>
+            <p> / </p>
+            <p>
+              {{ `${(disk.total / 1024 / 1024 / 1024).toFixed(2)} GB` }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
