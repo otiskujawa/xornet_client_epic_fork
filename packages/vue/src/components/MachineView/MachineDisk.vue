11960c0dedbb98123ae06fc93ab3290eb02eae32
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-03-09 03:12:46 -0500

    Revert "updated colors for disk/ram usages"
    This reverts commit fcc44ae7d8c44193d854d0b75f3952a133d569f1.

--- packages/vue/src/components/MachineView/MachineDisk.vue
+++ packages/vue/src/components/MachineView/MachineDisk.vue
@@ -22,8 +22,8 @@
           {{ ( disk.used / disk.total * 100).toFixed(2) }}% used
         </p>
       </div>
-      <div class="mt-1 relative flex overflow-hidden items-center bg-100000mbps bg-opacity-10 w-full min-h-1 h-1">
-        <div class="border-transparent border-r-100000mbps border-1px h-full bg-100000mbps bg-opacity-50 duration-100 " :style="`width: ${ disk.used / disk.total * 100 }%`" />
+      <div class="mt-1 relative flex overflow-hidden items-center bg-primary-400 bg-opacity-10 w-full min-h-1 h-1">
+        <div class="border-transparent border-r-primary-400 border-1px h-full bg-primary-400 bg-opacity-50 duration-100 " :style="`width: ${ disk.used / disk.total * 100 }%`" />
         <div class="w-full h-full duration-100" :style="`width: ${ 100 - (disk.used / disk.total * 100) }%`" />
       </div>
     </div>
--- packages/vue/src/components/MachineView/MachineProcessorUsageBars.vue
+++ packages/vue/src/components/MachineView/MachineProcessorUsageBars.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-1 pl-2 h-full">
     <div v-for="i of processor.usage.length" :key="i" class="flexcol justify-end w-2 bg-primary-400 bg-opacity-25 h-full rounded-full">
-      <div class="duration-1000 bg-primary-400 w-full rounded-full" :style="`height: ${processor.usage[i-1]}%`" />
+      <div class="duration-250 bg-primary-400 w-full rounded-full" :style="`height: ${processor.usage[i-1]}%`" />
     </div>
   </div>
 </template>
