85f71f65098be49ebdf30d707864beb2f475abb6
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-03-09 14:10:33 -0500

    show used swap instead of allocated
    Co-Authored-By: minty <47675324+MININT2@users.noreply.github.com>
    Co-Authored-By: Connor <cimok2000@gmail.com>
    Co-Authored-By: Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>

--- packages/vue/src/components/MachineView/MachineMemoryComposition.vue
+++ packages/vue/src/components/MachineView/MachineMemoryComposition.vue
@@ -3,22 +3,22 @@
     <div class="justify-between flex gap-2 items-center">
       <p>Memory Composition</p>
       <div class="flex gap-2 items-center">
-        <p>Used</p>
-        <div class="legend bg-primary-400 bg-opacity-25" />
-        <p v-if="swap">
+        <p v-if="isShowingSwap">
           Swap
         </p>
-        <div v-if="swap" class="swap legend bg-primary-400 bg-opacity-25" />
+        <div v-if="isShowingSwap" class="swap legend bg-primary-400 bg-opacity-25" />
+        <p>Used</p>
+        <div class="legend bg-primary-400 bg-opacity-25" />
         <p>Free</p>
         <div class="legend bg-primary-400 bg-opacity-5" />
       </div>
     </div>
     <div class="relative whitespace-nowrap flex items-center bg-primary-400 bg-opacity-5 border-1 border-primary-400 w-full min-h-12 h-12">
+      <div v-if="isShowingSwap" class="edgeBorder swap flex items-center w-full justify-center h-full duration-250" :style="`width: ${ swap.used / totalMemoryCombined * 100 }%`">
+        {{ (swap!.used / 1024 / 1024).toFixed(2) }} GB
+      </div>
       <div class="edgeBorder flex items-center justify-center h-full bg-primary-400 bg-opacity-25 duration-250 " :style="`width: ${ memory.used / totalMemoryCombined * 100 }%`">
         {{ (memory.used / 1024 / 1024).toFixed(2) }} GB
-      </div>
-      <div v-if="swap" class="edgeBorder swap flex items-center w-full justify-center h-full duration-250" :style="`width: ${ swap.total / totalMemoryCombined * 100 }%`">
-        {{ (swap.total / 1024 / 1024).toFixed(2) }} GB
       </div>
       <div class="flex items-center w-full justify-center h-full duration-250" :style="`width: ${ 100 - (swap ? ((memory.used / totalMemoryCombined * 100) + (swap.total / totalMemoryCombined * 100)) : ((memory.used / totalMemoryCombined * 100))) }%`">
         {{ ((memory.total - memory.used) / 1024 / 1024).toFixed(2) }} GB
@@ -32,7 +32,7 @@ import { computed } from "vue";
 import type { IRAM, ISwap } from "/@/types/api/machine";
 const props = defineProps<{memory: IRAM; swap?: ISwap}>();
 const totalMemoryCombined = computed(() => props.swap?.total ? props.memory.total + props.swap?.total : props.memory.total);
-
+const isShowingSwap = computed(() => props.swap && props.swap.used / 1024 / 1024 > 0);
 </script>
 
 <style scoped>
