1a2c34fbc3ce7f91f428c537c25e446618f1d6ce
N1kO23 <niko.huuskonen.00@gmail.com>
2022-03-14 16:30:59 +0200

    Made profiles a bit nicer

--- packages/vue/src/views/Dashboard/ProfileView.vue
+++ packages/vue/src/views/Dashboard/ProfileView.vue
@@ -1,30 +1,43 @@
 <template>
   <div v-if="user" class="w-full h-full flexcol bg-black bg-opacity-25">
-    <top-bar />
-    <div class="w-full h-full px-4">
+    <div class="w-full h-full">
       <div
-        class="p-4 flex items-center justify-center"
-      >
-        <avatar :user="user" class="w-8rem" />
-      </div>
-      <p class="text-4xl text-center">
-        {{ user.username }}
-      </p>
-      <br>
-      {{ user.biography }}
-      <br>
-      UUID: {{ user.uuid }}
-      <br>
-      Account created: {{ new Date(user.created_at).toLocaleString() }}
-      <br>
-      Edited: {{ new Date(user.updated_at).toLocaleString() }}
-      <br>
-      <div v-if="user.uuid === state.users.getMe().uuid">
-        Avatar URL: <base-input
-          v-model="avatar_url"
-          :placeholder="user.avatar"
-          @change="updateAvatar(avatar_url)"
-        />
+        class="w-full h-18rem bg-cover bg-norepeat drag" :style="`
+      background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.25) 100%),
+      url(${banner_url});`"
+      />
+      <div class="px-32">
+        <div
+          class="flex"
+        >
+          <avatar :user="user" class="w-32 transform -translate-y-3rem" />
+          <p class="text-4xl text-center p-4 px-8">
+            {{ user.username }}
+          </p>
+        </div>
+        {{ user.biography }}
+        <br>
+        UUID: {{ user.uuid }}
+        <br>
+        Account created: {{ new Date(user.created_at).toLocaleString() }}
+        <br>
+        Edited: {{ new Date(user.updated_at).toLocaleString() }}
+        <br>
+        <br>
+        <div v-if="user.uuid === state.users.getMe().uuid">
+          Avatar URL: <base-input
+            v-model="avatar_url"
+            :placeholder="user.avatar"
+            @change="updateAvatar(avatar_url)"
+          />
+        </div>
+        <div v-if="user.uuid === state.users.getMe().uuid">
+          Banner URL: <base-input
+            v-model="banner_url"
+            :placeholder="user.banner"
+            @change="updateBanner(banner_url)"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -34,7 +47,6 @@ import { computed, onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useState } from "/@/app";
 import BaseInput from "/@/components/base/BaseInput.vue";
-import TopBar from "/@/components/TopBar.vue";
 import Avatar from "/@/components/user/Avatar.vue";
 const router = useRouter();
 const route = useRoute();
@@ -47,9 +59,14 @@ const user = computed(() => {
 });
 // This is kinda dumb
 const avatar_url = ref(user.value?.avatar || "");
+const banner_url = ref(user.value?.banner || "");
 const updateAvatar = (url: string) => {
 	if (user.value)
 		state.users.updateAvatar(url);
+};
+const updateBanner = (url: string) => {
+	if (user.value)
+		state.users.updateBanner(url);
 };
 
 onMounted(async() => {
