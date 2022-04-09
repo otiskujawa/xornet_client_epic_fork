ab23b704e4908460d2e45caba913d83260138293
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-09 16:54:16 +0300

    we do a bit of customization

--- packages/vue/src/components/OptionField.vue
+++ packages/vue/src/components/OptionField.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="option ">
-    <div class="flexcol gap-1">
-      <label>
-        <label v-if="newFeature" class="w-min rounded-full text-xs bg-primary-400 px-2.5 py-0.5 ">NEW</label>
-        {{ label }}
-      </label>
-      <p v-if="description" class=" text-xs opacity-40">
-        {{ description }}
-      </p>
+  <div class="flexcol p-2 w-full bg-background-300 rounded-8px gap-2 ">
+    <div class="option">
+      <div class="flexcol  gap-1">
+        <label>
+          <label v-if="newFeature" class="w-min rounded-full text-xs bg-primary-400 px-2.5 py-0.5 ">NEW</label>
+          {{ label }}
+        </label>
+        <p v-if="description" class="font-light text-xs opacity-40">
+          {{ description }}
+        </p>
+      </div>
+      <slot />
     </div>
-    <slot />
+    <slot name="example" />
   </div>
 </template>
 
--- packages/vue/src/views/Dashboard/SettingsView.vue
+++ packages/vue/src/views/Dashboard/SettingsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-full overflow-y-scroll items-center flexcol bg-black bg-opacity-25">
     <top-bar />
-    <div class="optionList">
+    <div class="optionList flexcol ">
       <p>
         Account
       </p>
@@ -26,7 +26,43 @@
         <base-switch v-model="state.settings.general.use_single_color_for_switch_lights" />
       </option-field>
       <option-field new-feature label="Minimum Blink Speed" description="Change the minimum transfer speed (in Mbps) for a interface light to blink">
-        <base-input v-model="state.settings.general.minimum_blink_speed" :placeholder="state.settings.general.minimum_blink_speed" class="text-white w-24" />
+        <base-input v-model="state.settings.general.minimum_blink_speed" placeholder="0.001, 0.01" class="text-white w-32" />
+        <template #example>
+          <div class="flex gap-2 justify-between">
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'a', s: 100, rx: 0, tx: 1000 }" />
+              <p>0.001 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'b', s: 100, rx: 0, tx: 10000 }" />
+              <p>0.01 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'c', s: 100, rx: 0, tx: 100000 }" />
+              <p>0.1 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'd', s: 100, rx: 0, tx: 1000000 }" />
+              <p>1 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'e', s: 100, rx: 0, tx: 10000000 }" />
+              <p>10 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'f', s: 1000, rx: 0, tx: 100000000 }" />
+              <p>100 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'g', s: 10000, rx: 0, tx: 1000000000 }" />
+              <p>1000 Mbps</p>
+            </div>
+            <div class="exampleNic flexcol">
+              <network-interface :iface="{ n: 'g', s: 100000, rx: 0, tx: 10000000000 }" />
+              <p>10000 Mbps</p>
+            </div>
+          </div>
+        </template>
       </option-field>
       <option-field v-if="isElectron()" description="Adjust the window's opacity" label="Background Opacity">
         <div class="flex gap-2 items-center">
@@ -72,6 +108,7 @@ import OptionField from "/@/components/OptionField.vue";
 import TopBar from "/@/components/TopBar.vue";
 import { isElectron, logout } from "/@/services/logic";
 import BaseInput from "/@/components/base/BaseInput.vue";
+import NetworkInterface from "/@/components/NetworkInterface.vue";
 const state = useState();
 const deleteAccount = () => state.users.deleteAccount().then(() => logout());
 </script>
@@ -79,9 +116,16 @@ const deleteAccount = () => state.users.deleteAccount().then(() => logout());
 <style scoped lang="postcss">
 
 .optionList {
-  @apply  h-full text-left flexcol items-center w-full max-w-224 gap-2 text-12px text-text px-8;
-  & p {
+  @apply h-full text-left gap-4 items-center w-full max-w-224 gap-2 text-12px text-text px-8;
+  & > p {
     @apply text-xl w-full pt-16;
+  }
+}
+
+.exampleNic {
+  @apply bg-background-400 font-light gap-4 items-center justify-center overflow-hidden p-4 rounded-8px;
+  & > p {
+    @apply text-white text-center text-xs text-opacity-50;
   }
 }
 </style>
