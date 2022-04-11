94598d49537fd35f236ce4103e69800c1ff9e213
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-11 09:58:05 +0300

    clicky click

--- packages/vue/src/components/base/BaseButton.vue
+++ packages/vue/src/components/base/BaseButton.vue
@@ -1,7 +1,18 @@
 <template>
   <button
     :class="!borderless && 'border-1 border-opacity-50  hover:border-opacity-75 border-primary-400 '"
-    class="active:shadow-xl no-drag hover:bg-primary-400 hover:bg-opacity-25 text-primary-300   ring-white filter disabled:saturate-20 disabled:cursor-not-allowed hover cursor-pointer flex items-center gap-2 transition duration-200  rounded-4px px-4 py-2 focus-within:ring-2 "
+    class="
+      active:shadow-xl
+      active:scale-90 transform no-drag
+      hover:bg-primary-400
+      hover:bg-opacity-25 text-primary-300
+      ring-white filter
+      disabled:saturate-20
+      disabled:cursor-not-allowed
+      cursor-pointer flex items-center
+      gap-2 transition duration-100
+      rounded-4px px-4 py-2
+    "
     @click="soundManager.playClick();"
     @mouseenter="soundManager.playHover()"
   >
