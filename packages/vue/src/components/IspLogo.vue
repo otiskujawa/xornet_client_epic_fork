5557ec6b0f16d0c79e4df11cea86f5bd29b4e445
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2022-04-15 22:28:58 +0300

    Update IspLogo.vue

--- packages/vue/src/components/IspLogo.vue
+++ packages/vue/src/components/IspLogo.vue
@@ -2,25 +2,40 @@
   <img class="w-full max-w-6 max-h-6" :src="`/public/isps/${image}.svg`" :alt="image">
 </template>
 
-<script setup lang="ts">import { computed } from "vue";
-
+<script setup lang="ts">
+import { computed } from "vue";
 const props = defineProps<{name: string}>();
+const KNOWN_ISPS = [
+	"at&t",
+	"comcast",
+	"cox",
+	"dna",
+	"dom.ru",
+	"elisa",
+	"fios",
+	"o2",
+	"ote",
+	"rt.ru",
+	"telia",
+	"telus",
+	"timewarnercable",
+	"tmobile",
+	"turner",
+	"virgin",
+	"vodafone",
+	"wind",
+	"xfinity",
+	"xs4all",
+];
 
 const image = computed(() => {
-	switch (props.name) {
-		case "Polkomtel Sp. z o.o.":
-			return "polkomtel";
-		case "MEDIANOVA INTERNET HIZMETLERI VE TICARET ANONIM SIRKETI":
-			return "medianova";
-		case "TURKCELL":
-			return "turkcell";
-		case "Cosmote Mobile Telecommunications S.A.":
-			return "cosmote";
-		case "Vodafone Libertel B.V.":
-			return "vodafone";
-		default:
-			return "vodafone";
+	const lowercase = props.name.toLowerCase();
+	for (let i = 0; i < KNOWN_ISPS.length; i++) {
+		const knownIsp = KNOWN_ISPS[i];
+		if (lowercase.includes(knownIsp))
+			return knownIsp;
 	}
+	return "unknown";
 });
 
 </script>
